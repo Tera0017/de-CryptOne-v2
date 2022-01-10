2a0d347c6dbe874483a127739cc3e762a66ddc0f
Tera0017 <59655709+Tera0017@users.noreply.github.com>
2022-01-10 21:01:44 +0100

    Update decryptexecutable.py
    update covering more samples currently 16k (1.10.2022)

--- modules/decryptexecutable.py
+++ modules/decryptexecutable.py
@@ -50,14 +50,50 @@ class DecryptExecutable:
         except ValueError:
             return self.filedata[idx - 100: idx + 600]
 
-    def get_vars2(self):
-        def get_matches(zrules, data=None):
-            for rule in zrules:
-                matches = match_rule(self.rules[rule], self.filedata if data is None else data)
-                if matches:
-                    return matches, rule
-            return []
+    def get_matches(self, zrules, data=None):
+        for rule in zrules:
+            matches = match_rule(self.rules[rule], self.filedata if data is None else data)
+            if matches:
+                return matches, rule
+        return []
 
+    def offset_vars(self, data, vars):
+        matches, rule = self.get_matches([k for k in self.rules.keys() if '$codev2' in k], data)
+        opr1 = b'\xC7\x05'
+        for match in matches:
+            opcodes = match[2]
+            chunk_offset = opcodes[2: 2 + 4]
+            z = opr1 + chunk_offset
+            idx = data.index(z) + len(z)
+            vars['chunk_size'] = struct.unpack('I', data[idx: idx + 4])[0]
+            junk_offset = opcodes[-11: -11 + 4]
+            z = opr1 + junk_offset
+            if z in data:
+                idx = data.index(z) + len(z)
+                vars['junk_size'] = struct.unpack('I', data[idx: idx + 4])[0]
+            else:
+                vars['junk_size'] = 0x0
+            data2 = data[match[0] + len(match[2]) + 1:]
+            break
+        matches, rule = self.get_matches([k for k in self.rules.keys() if '$codev3' in k], data2)
+        for match in matches:
+            opcodes = match[2]
+            xor_add_key_offset = opcodes[2: 2 + 4]
+            z = opr1 + xor_add_key_offset
+            idx = data.index(z) + len(z)
+            vars['xor_add_key'] = struct.unpack('I', data[idx: idx + 4])[0]
+            if rule == '$codev31':
+                xor_key_offset = opcodes[8: 8 + 4]
+                z = opr1 + xor_key_offset
+                idx = data.index(z) + len(z)
+                vars['xor_key'] = struct.unpack('I', data[idx: idx + 4])[0]
+                vars['minus_xor'] = -1 * struct.unpack('I', opcodes[-5: -5 + 4])[0]
+                vars['xor_add_key'] += vars['minus_xor']
+            else:
+                vars['xor_key'] = struct.unpack('I', opcodes[-5: -5 + 4])[0]
+        return vars
+
+    def get_vars2(self):
         def clean_data(data):
             rule = '{6A ?? E8 ?? ?? ?? ??}'
             for match in match_rule(rule, data):
@@ -69,7 +105,7 @@ class DecryptExecutable:
             'xor_key': 0x0, 'xor_add_key': 0x0, 'minus_xor': 0x0,
         }
         # PART I loop
-        matches, rule = get_matches([k for k in self.rules.keys() if '$code1' in k])
+        matches, rule = self.get_matches([k for k in self.rules.keys() if '$code1' in k])
         opcs = []
         for match in matches:
             idx = match[0]
@@ -87,10 +123,13 @@ class DecryptExecutable:
         opr2 = b'\x89\x45'
         # PART II chunk/junk
         try:
-            matches, rule = get_matches([k for k in self.rules.keys() if '$code2' in k], data)
+            matches, rule = self.get_matches([k for k in self.rules.keys() if '$code2' in k], data)
         except ValueError:
             data = clean_data(data)
-            matches, rule = get_matches([k for k in self.rules.keys() if '$code2' in k], data)
+            try:
+                matches, rule = self.get_matches([k for k in self.rules.keys() if '$code2' in k], data)
+            except ValueError:
+                return self.offset_vars(data, vars)
         for match in matches:
             r2addr = match[0]
             opcodes = match[2]
@@ -110,7 +149,7 @@ class DecryptExecutable:
             break
         # PART III xor
         try:
-            matches, rule = get_matches([k for k in self.rules.keys() if '$code3' in k], data)
+            matches, rule = self.get_matches([k for k in self.rules.keys() if '$code3' in k], data)
             for match in matches:
                 opcodes = match[2]
                 if rule == '$code31':
@@ -150,6 +189,8 @@ class DecryptExecutable:
                 if b'\x83\xE8' in ndata:
                     idx = ndata.index(b'\x83\xE8') + 2
                     vars['minus_xor'] = -1 * ndata[idx]
+                elif b'\x48\x03' in ndata:
+                    vars['minus_xor'] = -1
                 else:
                     rule = '{(8B ?5| 03 ?5| 89 ?5) ?? 2D [4] (8B ?5| 03 ?5| 89 ?5)}'
                     matches = match_rule(rule, ndata)
@@ -208,7 +249,11 @@ class DecryptExecutable:
         message("Resource Name: " + resource['name'])
         message("Resource Size: " + hex(resource['size']).upper())
         decr_pckg = {'Decryption': variables, 'Resource': resource}
-        return mz_decr[idx: idx + size], decr_pckg
+        try:
+            return mz_decr[idx: idx + size], decr_pckg
+        except UnboundLocalError:
+            message(ERRORS['03'])
+            return None
 
 
 class DecryptExecutable86(DecryptExecutable):
@@ -218,6 +263,9 @@ class DecryptExecutable86(DecryptExecutable):
             '$code12': '{C7 45 ?? ?? 00 00 00 (68 ?? ?? ?? ??| 6A ??) E8 [4] FF [2] 81 [6] 7? ?? (68| 8B| C7)}',
             '$code13': '{C7 45 [4] 00 (68 ?? ?? ?? ??| 6A ??) E8 [4] FF [2] (81| 83) [3-6] 7? ?? (68| 8B| C7)}',
             '$code14': '{C7 45 [4] 00 [5-10] E8 [4] FF [2] (81| 83) [3-6] 7? ?? (68| 8B| C7)}',
+            '$code15': '{C7 45 [5] C7 45 [5] C7 45 [5] C7 45 [5] FF 45 ?? 81 [6] 7? ?? 6A}',
+            '$code16': '{C7 45 [4] 00 E8 [4] FF [2] (81| 83) [3-6] 7? ?? (68| 8B| C7)}',
+            '$code17': '{C7 45 [5] 81 [6] 7? [0-3] E8 [4-20] 81 [6] 7? ?? ??}',
             '$code2': '{8B 4D ?? E8 [4] [3-30] 01 [2] 8B 45 ?? 01 45 ?? 8B 45 ?? 01 45 ?? E?}',
             '$code21': '{8B 4D ?? E8 [0-34] 03 [0-32] (03 ?? ??| 8B 45 ??) ?? ?? ?? ??}',
             '$code22': '{03 45 ?? 50 E8 [4-60] 8B 45 ?? 01 45 ?? EB}',
@@ -227,6 +275,10 @@ class DecryptExecutable86(DecryptExecutable):
             '$code33': '{8B 45 ?? 03 45 ?? 89 45 ?? [6] 01 02 [10-60] 8B 45 ?? 03}',
             '$code34': '{8B 55 ?? 03 55 ?? 03 55 [0-6] 33}',
             '$code35': '{(?? ?? ?? 8B D8| 00 00) 8B 45 ?? 03 45 ?? (03 D8| 89) [5-70] 8B 45 ?? 03 45}',
+
+            '$codev2': '{8B ?? [4] E8 [4] [0-50] (03| 01) [0-50] EB}',
+            '$codev3': '{8B ?5 [4] 81 ?? [4] 03}',
+            '$codev31': '{?? A1 [4] 03 [5] 2D [4] 03}',
         }
         DecryptExecutable.__init__(self, filepath, 0x32, rules)
 
